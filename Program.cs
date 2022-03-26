cfa96350a4b1d57e805e46d7fda61356addcc438
sanjay <ssah6694@gmail.com>
2022-03-27 00:49:10 +0530

    [sanjay]Added: Stack list peak and pop file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UC2_Stack_PeakAndPop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stacking obj = new Stacking();
+            obj.Push(70);
+            obj.Push(30);
+            obj.Push(56);
+            obj.Display();
+
+            Console.WriteLine("picked item from Top is={0}",obj.peak());
+            obj.Pop();
+            Console.WriteLine("picked item from Top is={0}", obj.peak());
+            obj.Pop();
+            Console.WriteLine("picked item from Top is={0}", obj.peak());
+            obj.Pop();
+            obj.peak();
+        }
+    }
+}
---
+++ Stacking.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC2_Stack_PeakAndPop
+{
+    public class Stacking
+    {
+
+        internal Node top;
+
+        public bool Push(int data)
+        {
+            Node n = new Node(data);
+            if (top == null)
+            {
+                top = n;
+                Console.WriteLine("{0} inserted into the Stack", n.data);
+                return true;
+            }
+            else
+            {
+                n.next = top;
+                top = n;
+                Console.WriteLine("{0} inserted into the Stack", n.data);
+                return true;
+            }
+        }
+
+        public void Pop()
+        {
+            if(top==null)
+            {
+                Console.WriteLine("Nothing is in the Stack");
+            }
+            top = top.next;
+        }
+
+        public bool Empty()
+        {
+            return top == null;
+        }
+
+        public int peak()
+        {
+            if(!Empty())
+            {
+                return top.data;
+            }
+            else
+            {
+                Console.WriteLine("Stack is Empty");
+                return -1;
+            }
+        }
+
+
+        public void Display()
+        {
+            Node t = top;
+            if (t == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+            }
+            while (t != null)
+            {
+                Console.WriteLine(t.data + " ");
+                t = t.next;
+            }
+        }
+    }
+}
