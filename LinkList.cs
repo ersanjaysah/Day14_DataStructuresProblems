e681400c999125c5f5cc990b0d3c2ed3f9012c29
sanjay <ssah6694@gmail.com>
2022-03-26 13:53:46 +0530

    [sanjay]Added: link list UC1 file

---
+++ LinkList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1_SimpleLinkList
+{
+    public class LinkList
+    {
+        internal Node head;
+
+        public bool Add(int data)
+        {
+            Node n = new Node(data);
+            if (head==null)
+            {
+                head = n;
+                Console.WriteLine("{0} inserted into the link list",n.data);
+                return true;
+            }
+            else
+            {
+                n.next = head;
+                head = n;
+                Console.WriteLine("{0} inserted into the link list",n.data);
+                return true;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC1_SimpleLinkList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkList list = new LinkList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+        }
+    }
+}
