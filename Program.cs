b4feac4d7f1b3aea1aecaa3d9d465dae256993a3
sanjay <ssah6694@gmail.com>
2022-03-26 14:04:44 +0530

    [sanjay]Added: link list in Inserting data file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC4_InsertingData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Add(56);
+            list.Insert(1,30);
+            list.Display();
+        }
+    }
+}
