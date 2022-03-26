e0ab96f2f74b4d04c1b08d345d592cce8cd1e1ad
sanjay <ssah6694@gmail.com>
2022-03-27 00:44:57 +0530

    [sanjay]Added: Stack list file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UC1_CreateAStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack obj = new Stack();
+            obj.Push(70);
+            obj.Push(30);
+            obj.Push(56);
+            obj.Display();
+        }
+    }
+}
