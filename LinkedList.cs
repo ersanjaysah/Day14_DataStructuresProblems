14e1205cdcb02e6dad45046bb037083135b0b72a
sanjay <ssah6694@gmail.com>
2022-03-26 14:17:21 +0530

    [sanjay]Added: link list in Searching file

---
+++ LinkedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC7_SearchLinkList
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        public bool Add(int data)
+        {
+            Node n = new Node(data);
+            {
+                if(head==null)
+                {
+                    head = n;
+                    Console.WriteLine("{0} inserted into the link list", n.data);
+                    return true;
+                }
+                else
+                {
+                    n.next = head;
+                    head = n;
+                    Console.WriteLine("{0} inserted into the link list", n.data);
+                    return true;
+                }
+            }
+        }
+
+        public bool Search(int data)
+        {
+           if (head == null)
+            
+                throw new NullReferenceException("List is Empty");
+            
+            Node t = head; int count = 0;
+            while (t != null)
+            {
+               count++;
+                if (t.data == data)
+                    
+                    return true;
+                    Console.WriteLine("{0} data searched", data);
+                    t = t.next;
+            }
+            return false;
+        }
+        
+        internal void Display()
+        {
+            Node t = head;
+            if (t == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+            }
+            while (t != null)
+            {
+                Console.WriteLine(t.data + " ");
+                t = t.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UC7_SearchLinkList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Search(30);
+
+            list.Display();
+        }
+    }
+}
