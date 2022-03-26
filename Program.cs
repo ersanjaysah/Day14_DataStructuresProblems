31e6a920d093a4bbca9cb7347fb3cf7df2bf818b
sanjay <ssah6694@gmail.com>
2022-03-27 00:55:26 +0530

    [sanjay]Added: Queue enqueued and dequeued file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UC4_Dequeue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            QueueOperation obj = new QueueOperation();
+            obj.Enqueue(70);
+            obj.Enqueue(30);
+            obj.Enqueue(56);
+            obj.Display();
+
+            Console.WriteLine("statrting element is: " + obj.front.key);
+            Console.WriteLine("Last element is: " + obj.rear.key);
+            Console.WriteLine("Dequeuing elements from the Queue");
+            obj.Dequeue();
+            obj.Dequeue();
+            obj.Dequeue();
+            Console.WriteLine("All elements are Dequeued");
+
+        }
+    }
+}
---
+++ QueueOperation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_Dequeue
+{
+    internal class QueueOperation
+    {
+        internal Node front,rear;
+        public void Enqueue(int key)
+        {
+            Node temp = new Node(key);
+
+            if (this.rear == null)
+            {
+                this.front = this.rear = temp;
+                Console.WriteLine("{0} Enqueued into the Queue",key);
+                return;
+            }
+            this.rear.next = temp;
+            Console.WriteLine("{0} Enqueued into the Queue",key);
+            this.rear = temp;
+        }
+
+        public void Dequeue()
+        {
+            if (this.front == null)
+            {
+                return;
+            }
+
+            Node temp = this.front;
+            this.front = this.front.next;
+            if (this.front == null)
+            {
+                this.rear = null;
+            }
+        }
+
+        internal void Display()
+        {
+            Node t = front;
+            if (t == null)
+            {
+                Console.WriteLine(" Queue is Empty");
+            }
+            while (t != null)
+            {
+                Console.WriteLine(t.key + " ");
+                t = t.next;
+            }
+        }
+    }
+}
