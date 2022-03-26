a369cbdc72b39ae211b578ac8d3b1cab72fbda04
sanjay <ssah6694@gmail.com>
2022-03-26 14:10:40 +0530

    [sanjay]Added: link list in Deleting data with pop method file

---
+++ LinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC6_DeleteLastWithPopMethod
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        public bool Add(int data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                Console.WriteLine("{0} inserted into the link list", n.data);
+                return true;
+            }
+            else
+            {
+                n.next = head;
+                head = n;
+                Console.WriteLine("{0} inserted into the link list", n.data);
+                return true;
+            }
+
+        }
+        public bool Pop(int index)
+        {
+            Node obj = head;
+            if(index==0)
+            {
+                head = head.next;
+                return true;
+            }
+            Node t = head, pre = null;
+            while (index > 0 && t!=null)
+            {
+                index--;
+                pre = t;
+                t = t.next;
+            }
+            if (index==0)
+            {
+                pre.next = t.next;
+                return true;
+            }
+            throw new NullReferenceException("Index is not in range");
+        }
+
+        internal void Display()
+        {
+            Node t = head;
+            if (t == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+            }
+            while (t != null)
+            {
+                Console.WriteLine(t.data + " ");
+                t = t.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UC6_DeleteLastWithPopMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Pop(2);
+            
+            list.Display();
+        }
+    }
+}
