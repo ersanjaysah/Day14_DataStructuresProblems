3b3054e9955312fa30cf5c3ba925ebc207dc64b2
sanjay <ssah6694@gmail.com>
2022-03-26 14:27:24 +0530

    [sanjay]Added: link list find Size file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC9_ShowSizeLinkList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Insert(2, 40);
+            list.Delete(40);
+            int a=list.Size();
+            list.Display();
+            Console.WriteLine("After deleting Size is=" + a);
+        }
+    }
+}
