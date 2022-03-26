c5596f2220429e9dc9347693b02060ad78a2a7b4
sanjay <ssah6694@gmail.com>
2022-03-26 14:24:22 +0530

    [sanjay]Added: link list inserting data file

---
+++ LinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC8_InsertingData40
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        public bool Add(int data)
+        {
+            Node n = new Node(data);
+            if (head==null)
+            {
+                head = n;
+                Console.WriteLine("{0} inserted into the link list", n.data);
+                return true;
+            }
+            else
+            {
+                n.next = head;
+                head = n;
+                Console.WriteLine("{0} inserted into the link list", n.data);
+                return true;
+            }
+        }
+
+        public bool Insert(int index, int data)
+        {
+            Node n = new Node(data);
+            if (index == 0)
+            {
+                n.next = head.next;
+                head = n;
+                Console.WriteLine("{0}Data inserted", n.data);
+                return true;
+            }
+
+            Node t = head, pre = null;
+            while (index > 0 && t != null)
+            {
+                index--;
+                pre = t;
+                t = t.next;
+            }
+            if (index == 0)
+            {
+                pre.next = n;
+                n.next = t;
+                Console.WriteLine("{0} Data inserted", n.data);
+                return true;
+            }
+            throw new NullReferenceException("index is not in range");
+        }
+
+        internal void Display()
+        {
+            Node t = head;
+            if (t == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+            }
+            while (t != null)
+            {
+                Console.WriteLine(t.data + " ");
+                t = t.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UC8_InsertingData40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList list = new LinkedList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Insert(2, 40);
+            list.Display();
+        }
+    }
+}
